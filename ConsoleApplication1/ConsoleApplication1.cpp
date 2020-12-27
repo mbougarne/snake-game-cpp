0552ef5a4fe9a3cab6bec59bcc2100a223bfc928
Mourad Bougarne <contact@mbougarne.me>
2020-12-27 18:11:24 +0100

    Add initial functionalities

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 
+bool isGameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+
+void setUp();
+void draw();
+void input();
+void logic();
+
 int main() {
+
+	setUp();
+
+	while (!isGameOver) {
+		draw();
+		input();
+		logic();
+	}
+
 	return 0;
+}
+
+void setUp() {
+	isGameOver = false;
+}
+
+void draw() {
+
+}
+
+void input() {
+
+}
+
+void logic() {
+
 }
